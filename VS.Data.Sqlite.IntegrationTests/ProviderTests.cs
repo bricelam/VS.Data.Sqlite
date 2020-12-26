3d275c49bb3bd2aa5765fb25e65fdefa8b8e38af
Brice Lambson <brice@bricelam.net>
2020-12-26 15:42:59 -0800

    Add an integration test

---
+++ VS.Data.Sqlite.IntegrationTests/ProviderTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.VisualStudio.Data.Core;
+using Microsoft.VisualStudio.Data.Sqlite.Properties;
+using Microsoft.VisualStudio.Shell;
+using Xunit;
+
+namespace VisualStudio.Data.Sqlite
+{
+    public class ProviderTests
+    {
+        [VsFact]
+        public void Is_registered()
+        {
+            var providerManager = (IVsDataProviderManager)ServiceProvider.GlobalProvider.GetService(typeof(IVsDataProviderManager));
+
+            var provider = Assert.Contains(new Guid("796A79E8-2579-4375-9E12-03A9E0D1FC02"), providerManager.Providers);
+
+            Assert.Equal("Microsoft.Data.Sqlite Provider", provider.Name);
+            Assert.Equal(Resources.DataProvider_Description, provider.Description);
+            Assert.Equal(Resources.DataProvider_DisplayName, provider.DisplayName);
+            Assert.Equal("Microsoft.Data.Sqlite", provider.GetProperty("InvariantName"));
+            Assert.Equal(Resources.DataProvider_ShortDisplayName, provider.ShortDisplayName);
+            Assert.Equal(new Guid("77AB9A9D-78B9-4BA7-91AC-873F5338F1D2"), provider.Technology);
+        }
+    }
+}
---
+++ VS.Data.Sqlite/Properties/InternalsVisibleTo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("VisualStudio.Data.Sqlite.IntegrationTests")]
