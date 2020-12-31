b890b7e44106a057f7249efb234d239ff93ab462
Brice Lambson <brice@bricelam.net>
2020-12-31 13:26:09 -0800

    Use just the file name in Server Explorer

---
+++ VS.Data.Sqlite/SqliteRootObjectSelector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Data;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Microsoft.VisualStudio.Data.Framework.AdoDotNet;
+using Microsoft.VisualStudio.Data.Services.SupportEntities;
+
+namespace Microsoft.VisualStudio.Data.Sqlite
+{
+    class SqliteRootObjectSelector : AdoDotNetRootObjectSelector
+    {
+        protected override IVsDataReader SelectObjects(string typeName, object[] restrictions, string[] properties, object[] parameters)
+        {
+            IVsDataReader dataReader;
+
+            var connection = (SqliteConnection)Site.GetLockedProviderObject();
+            try
+            {
+                Site.EnsureConnected();
+
+                var dataTable = new DataTable
+                {
+                    Columns =
+                    {
+                        { "DatabaseName" }
+                    },
+                    Rows =
+                    {
+                        new object[]
+                        {
+                            Path.GetFileNameWithoutExtension(connection.DataSource)
+                        }
+                    }
+                };
+
+                if (parameters?.Length == 1)
+                {
+                    ApplyMappings(dataTable, GetMappings((object[])((DictionaryEntry)parameters[0]).Value));
+                }
+
+                dataReader = new AdoDotNetTableReader(dataTable);
+            }
+            finally
+            {
+                Site.UnlockProviderObject();
+            }
+
+            return dataReader;
+        }
+    }
+}
