0e6b7e3faf6fc03430137caf830c0e6fa076e023
Brice Lambson <brice@bricelam.net>
2022-07-21 15:11:30 -0700

    Simplify SqliteSourceInformation

--- VS.Data.Sqlite/SqliteSourceInformation.cs
+++ VS.Data.Sqlite/SqliteSourceInformation.cs
@@ -8,35 +8,19 @@ namespace Microsoft.VisualStudio.Data.Sqlite
         public SqliteSourceInformation(IVsDataConnection connection)
             : base(connection)
         {
-            Initialize();
-        }
-
-        private void Initialize()
-        {
-            AddProperty(DefaultSchema);
-            AddProperty(DefaultCatalog, "main");
-            AddProperty(SupportsAnsi92Sql, true);
             AddProperty(SupportsQuotedIdentifierParts, true);
             AddProperty(IdentifierOpenQuote, "\"");
             AddProperty(IdentifierCloseQuote, "\"");
-            AddProperty(CatalogSeparator, ".");
-            AddProperty(CatalogSupported, true);
-            AddProperty(CatalogSupportedInDml, true);
-            AddProperty(SchemaSupported, false);
-            AddProperty(SchemaSupportedInDml, false);
-            AddProperty(SchemaSeparator, string.Empty);
-            AddProperty(ParameterPrefix, "$");
-            AddProperty(ParameterPrefixInName, true);
         }
 
         protected override object RetrieveValue(string propertyName)
         {
             switch (propertyName)
             {
-                case "DataSourceProduct":
+                case DataSourceProduct:
                     return "SQLite";
 
-                case "DataSourceVersion":
+                case DataSourceVersion:
                     return Connection.ServerVersion;
 
                 default:
